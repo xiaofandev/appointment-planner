886ba1f37f067d37638d0dce6a81a08df5629031
肖帆 <xiaofandev@126.com>
2024-01-07 18:39:35 +0800

    new feature: add appointment

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -8,7 +8,6 @@ const getTodayString = () => {
 };
 
 export const AppointmentForm = ({
-  contacts,
   title,
   setTitle,
   contact,
@@ -21,6 +20,61 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-group"></div>
+        <div className="form-group">
+          <label htmlFor="contact">Contact</label>
+          <input
+            type="text"
+            className="form-control"
+            id="contact"
+            placeholder="Contact"
+            value={contact}
+            onChange={e => setContact(e.target.value)}
+          />
+        </div>
+        <div className="form-group"></div>
+        <div className="form-group">
+          <label htmlFor="date">Date</label>
+          <input
+            type="date"
+            className="form-control"
+            id="date"
+            value={date || getTodayString()}
+            onChange={e => setDate(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="time">Time</label>
+          <input
+            type="time"
+            className="form-control"
+            id="time"
+            value={time}
+            onChange={e => setTime(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit  
+        </button>
+      </form>
+      <style jsx>{`
+       .form-group {
+          margin-bottom: 1rem;
+        }
+      `}</style>
+    </>
   );
 };
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,17 +3,27 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({ contacts, appointments, addAppointment }) => {
   /*
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
+    addAppointment({
+      title,
+      contact,
+      date,
+      time
+    });
    
   };
 
@@ -21,6 +31,17 @@ export const AppointmentsPage = () => {
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          title={title}
+          setTitle={setTitle}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={time}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
